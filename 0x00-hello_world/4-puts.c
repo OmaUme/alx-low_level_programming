73f5c375700fbb2864bf3c9ca46f8c51a7a9929d
OmaUme <onwukaglory39@gmail.com>
2023-09-07 16:07:09 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function is the entry point for the program and
+ * performs the main tasks of the program.
+ *
+ * Return: 0 on success, non-zero on failure
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
