1a8e3b8f5f261a8706587e17adc3988bd292b27b
OmaUme <onwukaglory39@gmail.com>
2023-09-13 02:58:35 -0700

    header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - start point of the program
+ * _putchar prints the result
+ * Return: 0
+ */
+int main(void)
+{
+char c[] = "_putchar";
+int ('a');
+for (a = 0; a < 8; a++)
+{
+_putchar(c[a]);
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readmes for 0x02 functions and nested loops
