17cbcc5fda04fbe8504a1275238f273a622bc953
OmaUme <onwukaglory39@gmail.com>
2023-09-14 06:25:01 -0700

    Positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,16 +3,14 @@
 #include <stdio.h>
 /**
  * positive_or_negative -assign a random number to the variables
- * Print a random num
+ * @i: interger
  * i > 0 print positive
  * i < 0 print negative
  * else print zero
- * Return: no return
+ * Return: 0
  */
-void positive_or_negative(void)
+void positive_or_negative(int i)
 {
-int i;
-i = 'n';
 if (i > 0)
 printf("%d is positive\n", i);
 else if (i < 0)
