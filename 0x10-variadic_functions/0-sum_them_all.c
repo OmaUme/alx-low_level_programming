487a809772ab2befbc4e920773b8d871b009dd08
OmaUme <onwukaglory39@gmail.com>
2023-11-10 09:56:44 -0800

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,3 +1,4 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
@@ -13,7 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum;
 	va_list list;
-
+	
+	if (n == 0)
+	return (0);
+		
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
