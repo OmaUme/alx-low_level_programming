3f45221a87dd325ae9af48ddcabbb3196bc1ec9c
OmaUme <onwukaglory39@gmail.com>
2023-11-29 07:36:35 -0800

    Copying files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <string.h>
-#include <errno.h>
 /**
 * append_text_to_file - Appends text at the end of a file
 * @filename: Name of the file
@@ -14,31 +13,30 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-int bytes_written;
+	int fd;
+	int nletters;
+	int rwr;
 
-if (filename == NULL || text_content == NULL)
-{
-return (-1); /* Return -1 if filename or text_content is NULL */
-}
+if (filename == NULL)
+return (-1);
+
 fd = open(filename, O_WRONLY | O_APPEND);
+
 if (fd == -1)
+return (-1);
+
+if (text_content)
+
 {
-if (errno == ENOENT)
-{
-return (-1); /* Return -1 if file does not exist */
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+	return (-1);
 }
-else
-{
-return (-1); /* Return -1 for permission or other errors */
-}
-}
-bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1)
-{
-close(fd);
-return (-1); /* Return -1 if write fails */
-}
+
 close(fd);
 return (1);
 }
