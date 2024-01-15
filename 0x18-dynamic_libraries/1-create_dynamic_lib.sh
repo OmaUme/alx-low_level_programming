6a12e6d8c0c831db98a2fcac75bc184460e2b486
OmaUme <onwukaglory39@gmail.com>
2024-01-15 06:59:40 -0800

    updated

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pendantic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 nm -D liball.so
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
